e01f40572325fe3485895945f8f40550683ba4b6
ColonelNosio <jakub.pabisz133@gmail.com>
2024-01-27 14:55:12 +0100

    Basic Clown Buff

--- Assets/Scripts/Entities/Clowns/BasicClown.cs
+++ Assets/Scripts/Entities/Clowns/BasicClown.cs
@@ -2,7 +2,7 @@ public class BaseClown : AEnemyEntity
 {
 
     public BaseClown()
-        : base("Clown", "You have been clowned.", health: 2, attackStrength: 1, movementSpeed: 1, attackRange: 1)
+        : base("Clown", "You have been clowned.", health: 3, attackStrength: 2, movementSpeed: 2, attackRange: 1)
     {
 
     }
