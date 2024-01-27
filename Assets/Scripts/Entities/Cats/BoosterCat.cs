1997b429d83b27a42f1da2a6cb6ccefa8dc25c2f
ColonelNosio <jakub.pabisz133@gmail.com>
2024-01-27 11:50:16 +0100

    Changed stats of BoosterCat.

--- Assets/Scripts/Entities/Cats/BoosterCat.cs
+++ Assets/Scripts/Entities/Cats/BoosterCat.cs
@@ -1,7 +1,7 @@
 public class BoosterCat : APlayerEntity
 {
     public BoosterCat()
-        : base("Cat with a ball of jarn.", "I've got a ball of jarn!", maxHealth: 50, attackStrength: 10, cost: 3)
+        : base("Cat with a ball of jarn.", "I've got a ball of jarn!", maxHealth: 2, attackStrength: 0, cost: 2)
     {
         
     }
