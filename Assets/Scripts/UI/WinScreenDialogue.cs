ba5a705726768e2f2bc2d84f53ad415652bc39be
DawidCyga <dawid.zagula23@gmail.com>
2024-01-27 22:44:41 +0100

    commitg

---
+++ Assets/Scripts/UI/WinScreenDialogue.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class WinScreenDialogue : MonoBehaviour
+{
+    [Header("Typing Parameters")]
+    [TextArea(2, 4)]
+    [SerializeField] private string[] _dialogueLines;
+    [SerializeField] private int _dialogueLineIndex;
+    [SerializeField] private float _typingSpeed;
+
+    [Header("References")]
+    [SerializeField] private GameObject _nextButtonObject;
+    [SerializeField] private Button _nextButton;
+    [SerializeField] private TextMeshProUGUI _dialogueText;
+
+    private void Start()
+    {
+        _dialogueText.text = string.Empty;
+        _nextButton.onClick.AddListener(() =>
+        {
+            if (_dialogueLineIndex >= _dialogueLines.Length - 1)
+            {
+                SceneManager.LoadScene(0);
+            }
+            else
+            {
+                _dialogueText.text = string.Empty;
+                _dialogueLineIndex++;
+                StartCoroutine(TypingRoutine());
+                _nextButtonObject.SetActive(false);
+            }
+        });
+
+        StartCoroutine(TypingRoutine());
+    }
+
+    private IEnumerator TypingRoutine()
+    {
+        for (int i = 0; i < _dialogueLines[_dialogueLineIndex].ToCharArray().Length; i++)
+        {
+            char c = _dialogueLines[_dialogueLineIndex].ToCharArray()[i];
+            _dialogueText.text += c;
+            yield return new WaitForSeconds(_typingSpeed);
+        }
+        _nextButtonObject?.SetActive(true);
+
+    }
+}
