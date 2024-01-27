ce971b72c0004420274fdd171fe73e039beb9d37
DawidCyga <dawid.zagula23@gmail.com>
2024-01-27 22:10:13 +0100

    WWW

---
+++ Assets/Scripts/Animations/BasicClownAnimation.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicClownAnimation : MonoBehaviour
+{
+    private Animator _animator;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    //WAITING FOR CLOWN METHOD
+}
