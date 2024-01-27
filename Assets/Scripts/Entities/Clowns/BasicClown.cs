e68a0c44995ac4afd3af8e25e5de43410829a9ce
ColonelNosio <jakub.pabisz133@gmail.com>
2024-01-27 12:29:03 +0100

    Changed clown stats.

--- Assets/Scripts/Entities/Clowns/BasicClown.cs
+++ Assets/Scripts/Entities/Clowns/BasicClown.cs
@@ -2,7 +2,7 @@ public class BaseClown : AEnemyEntity
 {
 
     public BaseClown()
-        : base("Clown", "You have been clowned.", health: 60, attackStrength: 8, movementSpeed: 1, attackRange: 1)
+        : base("Clown", "You have been clowned.", health: 2, attackStrength: 1, movementSpeed: 1, attackRange: 1)
     {
 
     }
