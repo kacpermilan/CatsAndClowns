05b3db51246b0403a41ecb92ac93dd1503741e65
kacpermilan <kacpermmilan@gmail.com>
2024-01-28 02:35:23 +0100

    feat: finish the PlayerUnitActions script

--- Assets/Scripts/EnemySpawner/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner/EnemySpawner.cs
@@ -32,7 +32,7 @@ public class EnemySpawner : MonoBehaviour
             {
                 for (int i = 0; i < _spawnedEnemies.Count; i++)
                 {
-                    if (!_spawnedEnemies[i].GetComponent<EnemyMover>().HasAttacked())
+                    if (!_spawnedEnemies[i].GetComponent<EnemySequence>().HasAttacked())
                     {
                         return;
                     }
